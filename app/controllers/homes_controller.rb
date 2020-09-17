34e41a29d1349fdec9a9242b543e19ac7486d8d0
vagrant <vagrant@localhost.localdomain>
2020-09-17 07:17:07 +0000

    Capistrano

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,15 @@
 class HomesController < ApplicationController
 
 def top
-
+	# @aves = []
+	# Toy.all.each do |toy|
+	# @aves.push toy.reviews.average(:rate)
+	# end
+	# @averages = Review.select("toy_id,@aves(rate) as @aves").group(:toy_id)
 end
-
 
 def about
 end
 
 end
+
