21abfb81d9cd866f8e54b814c4a964803f43b40b
vagrant <vagrant@localhost.localdomain>
2020-09-19 16:42:06 +0000

    ranking/favorite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def after_sign_in_path_for(resource)
+        flash[:notice] = "ログインに成功しました"
+        root_path  #　指定したいパスに変更
+    end
+
   protected
 
   def configure_permitted_parameters
@@ -11,4 +16,3 @@ class ApplicationController < ActionController::Base
 
 
 end
-
