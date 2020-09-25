6b5fff0c1a1804c25ce9a76d3c7856e03e153347
vagrant <vagrant@localhost.localdomain>
2020-09-25 08:11:50 +0000

    login/signupview

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
 def top
-    @toys = Toy.joins(:reviews).group(:toy_id).order('avg(reviews.rate) desc')
+    @toys = Toy.joins(:reviews).group(:toy_id).order('avg(reviews.rate) desc').limit(3)
 end
 
 def about
