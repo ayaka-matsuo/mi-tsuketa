af204aae7e46878a5bb243ac7e8f15b6f4087d6b
vagrant <vagrant@localhost.localdomain>
2020-09-29 07:14:24 +0000

    seedaddchild

--- db/seeds.rb
+++ db/seeds.rb
@@ -361,7 +361,7 @@ Child.create!(
 
 ChildTemperament.create!(
    child_id: '1',
-   :temperament_ids[] ['1', '3', '4']
+   :temperament_ids[] ['1' '3' '4']
 )
 
 ChildArchive.create!(
@@ -379,7 +379,7 @@ ChildArchive.create!(
 
 ChildArchiveTemperament.create!(
    child_archive_id: '1',
-   :temperament_ids[] ['1', '3', '4']
+   :temperament_ids[] ['1' '3' '4']
 )
 
 
